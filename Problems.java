26b4579536407cc0f9d7f711a641f57e62f8e7ac
Serge Skaf <makhoul.serj@gmail.com>
2023-01-24 19:55:57 +0200

    corrected version

---
+++ Problems.java
@@ -0,0 +1,8 @@
+public class Problems{
+	public static void main(String[] Args){
+
+		System.out.print("!!!!!!!!!!!!!!!!!!!!!!!");
+		System.out.println("Hello, there are a lot");
+		System.out.print("of problems");
+	}
+}
